dc07397f469ed50daff56397922f867409b1997a
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-04-12 10:01:52 +0200

    Add CSS on main page

--- client/src/components/main-page/MainPage.jsx
+++ client/src/components/main-page/MainPage.jsx
@@ -15,17 +15,30 @@ function MainPage() {
   }, []);
 
   return (
-    <div className="main-window">
-      <img
-        src={datas && datas[0]?.headshot_url}
-        className="pilotsPicture"
-        alt="PilotsPicture"
-      />
-      <div className="verstappenname">
-        <h1>{datas && datas[0]?.full_name}</h1>
+    <>
+      <div className="main-window">
+        <img
+          src={datas && datas[0]?.headshot_url}
+          className="pilot-picture"
+          alt="PilotPicture"
+        />
+        <div className="pilot-name">
+          <h1>{datas && datas[0]?.full_name}</h1>
+        </div>
+        <h2 className="infopilote">
+          LAPS 2/14 <br />
+          Position 5/20 <br />
+          weather : 🌧️
+        </h2>
+        <div className="chooseButtons">
+          <button type="button" className="buttonYes">OUI</button>
+          <button type="button" className="buttonNo">NON</button>
+        </div>
       </div>
-      <h2>{datas && datas[0]?.team_name}</h2>
-    </div>
+      <h3 className="hot">🔥HOT</h3>
+      <img src="./src/assets/images/star.png" alt="étoile" className="stars" />;
+      <button type="button" className="buttonBet">PARIER</button>;
+    </>
   );
 }
 
