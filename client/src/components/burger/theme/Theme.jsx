a7cddff91ec195ac169871fcb66588e0686a4651
Benoît Vandanjon <benoit@vandanjon.fr>
2024-04-24 17:02:52 +0200

    hotfix general css setup + finish Theme component

--- client/src/components/burger/theme/Theme.jsx
+++ client/src/components/burger/theme/Theme.jsx
@@ -1,7 +1,36 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../../contexts/ThemeContext";
+
+import driverA from "../../../assets/images/hamilton.png";
+import driverB from "../../../assets/images/leclerc.png";
+import driverC from "../../../assets/images/Angelina.png";
+
+import "./themestyle.css";
 import Burger from "../Burger";
 
 function Theme() {
-  return <Burger />;
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <>
+      <Burger />
+
+      <section className="app-container" data-theme={theme}>
+        <h1>THEME PAGE</h1>
+        <div className="drivers-container">
+          <button onClick={() => setTheme("driverA")}>
+            <img src={driverA} alt="" />
+          </button>
+          <button onClick={() => setTheme("driverB")}>
+            <img src={driverB} alt="" />
+          </button>
+          <button onClick={() => setTheme("driverC")}>
+            <img src={driverC} alt="" />
+          </button>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Theme;
